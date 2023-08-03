e9fe8fb7cc8ef1c95d7d1a72cb52e13ec2093fa4
silekey <46124608+silekey2@users.noreply.github.com>
2023-08-03 10:22:36 +0800

    添加checkpoint

---
+++ Assets/Script/Game/Module/CheckPoint/Components/CheckPoints.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Unity.Transforms;
+namespace SGame
+{
+    public class CheckPointData : IComponentData
+    {
+        // 检查点
+        public List<Vector3> Value;
+    }
+    
+    public class CheckPoints : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public void Convert(
+            Entity entity,
+            EntityManager dstManager,
+            GameObjectConversionSystem conversionSystem)
+        {
+            Transform[] all_transform = transform.GetComponentsInChildren<Transform>();
+            CheckPointData v = new CheckPointData();
+            v.Value = new List<Vector3>();
+            foreach (var t in all_transform)
+            {
+                if (t == transform)
+                    continue;
+                
+                v.Value.Add(t.transform.position);
+            }
+
+            dstManager.AddComponentData(entity, v);
+        }
+    }
+}
