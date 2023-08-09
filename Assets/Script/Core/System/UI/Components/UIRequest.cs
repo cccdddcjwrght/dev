bcb69816a0256ebd01e6717825c95014d5a6c050
silekey <46124608+silekey2@users.noreply.github.com>
2023-08-09 15:02:33 +0800

    添加基础配置

--- Assets/Script/Core/System/UI/Components/UIRequest.cs
+++ Assets/Script/Core/System/UI/Components/UIRequest.cs
@@ -25,7 +25,7 @@ namespace SGame.UI
         {
             Entity e = commandBuffer.CreateEntity();
             commandBuffer.AddComponent<UIRequest>(e);
-            var req = new UIRequest() { comName = name, pkgName = pkgName };
+            var req = new UIRequest() { comName = name, pkgName = pkgName, configId = 0};
             commandBuffer.SetComponent(e, req);
             return e;
         }
@@ -33,7 +33,23 @@ namespace SGame.UI
         public static Entity Create(EntityManager entityManager, string name, string pkgName)
         {
             Entity e = entityManager.CreateEntity(typeof(UIRequest));
-            entityManager.SetComponentData(e, new UIRequest() { comName = name, pkgName = pkgName });
+            entityManager.SetComponentData(e, new UIRequest() { comName = name, pkgName = pkgName,configId = 0});
+            return e;
+        }
+        
+        public static Entity Create(EntityCommandBuffer commandBuffer, int configId)
+        {
+            Entity e = commandBuffer.CreateEntity();
+            commandBuffer.AddComponent<UIRequest>(e);
+            var req = new UIRequest() { comName = null, pkgName = null, configId = configId};
+            commandBuffer.SetComponent(e, req);
+            return e;
+        }
+        
+        public static Entity Create(EntityManager entityManager, int configId)
+        {
+            Entity e = entityManager.CreateEntity(typeof(UIRequest));
+            entityManager.SetComponentData(e, new UIRequest() { comName = null, pkgName = null,configId = configId});
             return e;
         }
     }
--- Assets/Script/Game/Module/UICommon/UIPreprocess.cs
+++ Assets/Script/Game/Module/UICommon/UIPreprocess.cs
@@ -4,6 +4,7 @@ namespace SGame
 {
     public class UIPreprocess : IPreprocess
     {
+        // 初始化UI状态, 包括是否全屏等等
         public void Init(UIContext context)
         {
             
@@ -13,6 +14,13 @@ namespace SGame
         {
             comName = null;
             pkgName = null;
+            if (ConfigSystem.Instance.TryGet(configId, out GameConfigs.ui_resRowData ui))
+            {
+                comName = ui.Name;
+                pkgName = ui.PackageName;
+                return true;
+            }
+
             return false;
         }
     }
