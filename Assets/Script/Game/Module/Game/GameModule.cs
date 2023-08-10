92e83594366afd2b58c42ab21a70b8d55ef2ff3c
silekey <46124608+silekey2@users.noreply.github.com>
2023-08-10 17:13:00 +0800

    删除多余MAIN UI

--- Assets/Script/Game/Module/Game/GameModule.cs
+++ Assets/Script/Game/Module/Game/GameModule.cs
@@ -60,8 +60,6 @@ namespace SGame
         // 开始游戏
         IEnumerator StartGame()
         {
-            UI.UIRequest.Create(m_gameWorld.GetEntityManager(), "MainUI", "Main");
-            
             // 等待checkPoint 转换结束!
             yield return FiberHelper.Wait(1.0f);
             
