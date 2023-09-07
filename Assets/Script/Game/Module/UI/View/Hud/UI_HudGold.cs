03194a9caa99a98d3f4c6bdc2d2cfee0fe7f14be
silekey <46124608+silekey2@users.noreply.github.com>
2023-09-07 16:10:07 +0800

    提交遗漏代码

---
+++ Assets/Script/Game/Module/UI/View/Hud/UI_HudGold.cs
@@ -0,0 +1,25 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+using FairyGUI.Utils;
+
+namespace SGame.UI.Hud
+{
+    public partial class UI_HudGold : GComponent
+    {
+        public GProgressBar m_gold;
+        public const string URL = "ui://clbwsjawf7ke2";
+
+        public static UI_HudGold CreateInstance()
+        {
+            return (UI_HudGold)UIPackage.CreateObject("Hud", "HudGold");
+        }
+
+        public override void ConstructFromXML(XML xml)
+        {
+            base.ConstructFromXML(xml);
+
+            m_gold = (GProgressBar)GetChildAt(0);
+        }
+    }
+}
