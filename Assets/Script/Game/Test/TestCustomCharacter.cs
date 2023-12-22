acc5067e71004d8ba2f9219548918ac6e1b1951a
silekey <46124608+silekey2@users.noreply.github.com>
2023-12-22 16:17:55 +0800

    自定义角色-add-添加测试代码

---
+++ Assets/Script/Game/Test/TestCustomCharacter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fibers;
+using UnityEngine;
+
+public class TestCustomCharacter : MonoBehaviour
+{
+    private GameObject currentCharacter;
+    private CharacterGenerator generator;
+    private Fiber m_fiber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_fiber = new Fiber(CreateRandomCharacter(), FiberBucket.Manual);
+    }
+
+    IEnumerator CreateRandomCharacter()
+    {
+        if (currentCharacter != null)
+            GameObject.Destroy(currentCharacter);
+
+        while (CharacterGenerator.ReadyToUse == false)
+            yield return null;
+
+        var generator = CharacterGenerator.CreateWithRandomConfig();
+        while (generator.ConfigReady == false)
+            yield return null;
+        
+        currentCharacter = generator.Generate();
+        currentCharacter.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_fiber.Step();
+    }
+    
+}
