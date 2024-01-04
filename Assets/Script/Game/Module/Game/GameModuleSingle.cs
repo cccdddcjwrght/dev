d86843d3ae9a4a8316ef6ecf674953903df00395
silekey <46124608+silekey2@users.noreply.github.com>
2024-01-04 17:48:21 +0800

    添加游戏模块
        pick 03809bc 整理代码-add-remove system 自动创建
        pick dd56210 整理代码

---
+++ Assets/Script/Game/Module/Game/GameModuleSingle.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fibers;
+using log4net;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace SGame
+{
+    // 主要用于运行单机游戏逻辑
+    public class GameModuleSingle : IModule
+    {
+        private const string script = "Assets/BuildAsset/VisualScript/Prefabs/Game.prefab";
+
+        
+        public GameModuleSingle(
+            GameWorld       gameWorld,
+            ResourceManager resourceManager,
+            RandomSystem    randomSystem
+            )
+        {
+            m_resourceManager = resourceManager;
+            m_gameWorld       = gameWorld;
+        }
+        
+        public EntityManager EntityManager { get { return m_gameWorld.GetEntityManager(); } }
+
+        private const float MOVE_INTERVAL_TIME = 0.0f;
+        
+        public void Update()
+        {
+            m_fiber.Step();
+        }
+
+        IEnumerator Logic()
+        {
+            var prefab = m_resourceManager.LoadPrefab(script);
+            var go = GameObject.Instantiate(prefab);
+            
+            // 游戏逻辑
+            while (true)
+            {
+                // 防止进入死循环
+                yield return null;
+            }
+            
+            GameObject.Destroy(go);
+        }
+
+
+        public void Enter()
+        {
+            m_fiber           = new Fiber(Logic());
+        }
+
+        public void Shutdown()
+        {
+        }
+        
+        private GameWorld           m_gameWorld      ;
+        private ResourceManager     m_resourceManager;
+        private Fiber               m_fiber          ;
+        
+
+        // 游戏主逻辑
+        private static ILog log = LogManager.GetLogger("xl.Game.Main");
+        
+        private ItemGroup           m_userData;
+
+        private UserSetting         m_userSetting;
+    }
+}
