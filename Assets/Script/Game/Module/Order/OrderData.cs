3f38cd5e341f4c68f049ca7383e2c9d2e080c89d
silekey <46124608+silekey2@users.noreply.github.com>
2024-01-17 12:20:36 +0800

    订单系统-task1040- 订单系统基本流程框架

--- Assets/Script/Game/Module/Order/OrderData.cs
+++ Assets/Script/Game/Module/Order/OrderData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Numerics;
+using log4net;
 
 namespace SGame
 {
@@ -10,30 +11,199 @@ namespace SGame
     /// </summary>
     public enum ORDER_PROGRESS : uint
     {
-         ORDING         = 0, // 下单中
-         FOOD_MAKING    = 1, // 制作中
-         FOOD_MAKED     = 2, // 制作完成
+         WAIT           = 0, // 等待点餐
+         ORDING         = 1, // 下单中
+         FOOD_MAKING    = 2, // 制作中
+         FOOD_MAKED     = 3, // 制作完成
+         DISH_POINT     = 4, // 放置到
+         MOVETO_CUSTOM  = 5, // 移动到顾客身边
+         FINISH         = 6, // 订单完成
     }
     
     // 订单数据 
     public class OrderData
     {
-        public int      id;                 // 订单ID
-        public int      startTime;          // 下单时间
-        public int      cookTime;           // 制作时间
-        public int      finishTime;         // 订单完成时间
-        public int      customerID;         // 顾客
-        public int      orderMakerID;       // 下单服务员
-        public int      servicerID;         // 消单服务员
-        public int      cookerID;           // 厨师ID
+        private static ILog log = LogManager.GetLogger("game.order");
         
-        public bool     perfect;            // 是否完美菜品
-        public int      dishPointID;        // 放餐点
+        public int      id {  get; private set; }          // 订单ID
+        public float    startTime;                         // 下单时间
+        public int      cookTime;                          // 制作时间
+        public int      finishTime;                        // 订单完成时间
+        public int      customerID { get; private set; }   // 顾客
+        public int      servicerID { get; private set; }   // 服务员ID （包含下单和清单)
+        public int      cookerID { get; private set; }     // 厨师ID
+        
+        public bool     perfect { get; private set; }     // 是否完美菜品
+        public int      dishPointID { get; private set; } // 放餐点
 
-        public int      foodID;  // 菜品ID
+        public int      foodID { get; private set; } // 菜品ID
         
-        public BigInteger  price;    // 菜品价格
+        public BigInteger  price;           // 菜品价格
         
-        public ORDER_PROGRESS progress; // 订单进度
+        public ORDER_PROGRESS progress { get; private set; } // 订单进度
+        
+        
+        /// <summary>
+        /// 创建订单
+        /// </summary>
+        /// <param name="id">订单ID</param>
+        /// <param name="customerID">顾客ID</param>
+        /// <param name="ordermakerID">点单的服务员</param>
+        /// <param name="foodID">菜品ID</param>
+        /// <returns></returns>
+        public static OrderData Create(int id, int customerID)
+        {
+            OrderData v = new OrderData();
+            v.Clear();
+            v.id = id;
+            v.customerID = customerID;
+            return v;
+        }
+
+        public void Clear()
+        {
+             this.id            = 0;            // 订单ID
+             this.startTime     = 0;            // 下单时间
+             this.cookTime      = 0;            // 制作时间
+             this.finishTime    = 0;            // 订单完成时间
+             this.customerID    = 0;            // 顾客
+             this.servicerID    = 0;            // 服务员
+             this.cookerID      = 0;            // 厨师ID
+             this.perfect       = false;        // 是否完美菜品
+             this.dishPointID   = 0;            // 放餐点
+             this.foodID        = 0;            // 菜品ID
+             this.price         = 0;   // 菜品价格
+             this.progress      = ORDER_PROGRESS.ORDING; // 订单进度
+        }
+
+        /// <summary>
+        /// 服务员处理订单
+        /// </summary>
+        /// <param name="servicerID">服务员ID</param>
+        /// <param name="foodID">菜品ID</param>
+        /// <returns></returns>
+        public bool Ording(int servicerID, int foodID)
+        {
+            if (progress != ORDER_PROGRESS.WAIT)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            this.servicerID = servicerID;
+            this.foodID = foodID;
+            progress = ORDER_PROGRESS.ORDING;
+            return true;
+        }
+        
+        
+        /// <summary>
+        /// 厨师接单
+        /// </summary>
+        /// <param name="cookerID"></param>
+        /// <returns></returns>
+        public bool Take(int cookerID)
+        {
+            if (progress != ORDER_PROGRESS.ORDING)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            this.cookerID = cookerID;
+            progress = ORDER_PROGRESS.FOOD_MAKING;
+            return true;
+        }
+
+        /// <summary>
+        /// 厨师完成菜品制作
+        /// </summary>
+        /// <param name="cookerID"></param>
+        /// <returns></returns>
+        public bool CookFinish(int cookerID)
+        {
+            if (progress != ORDER_PROGRESS.FOOD_MAKING)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            if (this.cookerID != cookerID)
+            {
+                // 厨师不匹配
+                log.Error("cooker is not match!");
+                return false;
+            }
+
+            progress = ORDER_PROGRESS.FOOD_MAKED;
+            return true;
+        }
+
+        /// <summary>
+        /// 移动到放餐区
+        /// </summary>
+        /// <param name="cookerID">厨师ID</param>
+        /// <param name="dishPointID">餐区ID</param>
+        /// <returns></returns>
+        public bool PutToDishPoint(int cookerID, int dishPointID)
+        {
+            if (progress != ORDER_PROGRESS.FOOD_MAKED)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            if (this.cookerID != cookerID)
+            {
+                log.Error("cookerid not match!");
+                return false;
+            }
+
+            this.dishPointID = dishPointID;
+            this.progress = ORDER_PROGRESS.DISH_POINT;
+            return true;
+        }
+
+        /// <summary>
+        /// 移动到顾客身边
+        /// </summary>
+        /// <param name="servicerID">服务员ID</param>
+        /// <returns></returns>
+        public bool MoveToCustom(int servicerID)
+        {
+            if (progress != ORDER_PROGRESS.DISH_POINT && progress != ORDER_PROGRESS.FOOD_MAKED)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            this.servicerID = servicerID;
+            this.progress = ORDER_PROGRESS.MOVETO_CUSTOM;
+            return true;
+        }
+
+        /// <summary>
+        /// 完成订单
+        /// </summary>
+        /// <param name="servicerID"></param>
+        /// <returns></returns>
+        public bool Finish(int servicerID)
+        {
+            if (progress != ORDER_PROGRESS.MOVETO_CUSTOM)
+            {
+                log.Error("order progress not match!");
+                return false;
+            }
+
+            if (this.servicerID != servicerID)
+            {
+                // 服务员不匹配， 完成订单的人必须是端盘子的人
+                log.Error("ServicerID not match !"); 
+                return false;
+            }
+
+            this.progress = ORDER_PROGRESS.FINISH;
+            return true;
+        }
     }
 }
--- Assets/Script/Game/Module/Order/OrderManager.cs
+++ Assets/Script/Game/Module/Order/OrderManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using log4net;
 using SGame;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,26 +10,60 @@ namespace SGame
     public class OrderManager : Singleton<OrderManager>
     {
         private int lastOrderID = 0;
+
+        private static ILog log = LogManager.GetLogger("game.order");
         
         /// <summary>
         /// 订单数据
         /// </summary>
-        private Dictionary<int, OrderData> m_datas = new Dictionary<int, OrderData>();
+        private Dictionary<int, OrderData>  m_datas     = new Dictionary<int, OrderData>();
+        private List<int>                   m_caches    = new List<int>();
 
         /// <summary>
-        /// 新的订单
+        /// 清空订单数据
         /// </summary>
-        private List<int>                  m_newOrders;
-
-        /// <summary>
-        /// 在餐牌里的
-        /// </summary>
-        private List<int>                  m_inDishOrders;
-        
         public void Clear()
         {
             m_datas.Clear();
+            m_caches.Clear();
             lastOrderID = 0;
+        }
+
+        /// <summary>
+        /// 根据进度查找订单
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        public List<int> FindOrders(ORDER_PROGRESS progress)
+        {
+            m_caches.Clear();
+            foreach (var item in m_datas.Values)
+            {
+                if (item.progress == progress)
+                {
+                    m_caches.Add(item.id);
+                }
+            }
+
+            return m_caches;
+        }
+
+        /// <summary>
+        /// 获得第一个订单
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        public int FindFirstOrder(ORDER_PROGRESS progress)
+        {
+            foreach (var item in m_datas.Values)
+            {
+                if (item.progress == progress)
+                {
+                    return item.id;
+                }
+            }
+
+            return 0;
         }
 
         /// <summary>
@@ -38,11 +73,41 @@ namespace SGame
         /// <param name="makerID"></param>
         /// <param name="foodID"></param>
         /// <returns></returns>
-        public OrderData Create(int customID, int makerID, int foodID)
+        public OrderData Create(int customID)
         {
-            ///EventBus.Trigger(EventHooks.Custom, );
-            //EventManager.Instance.Trigger((int)GameEvent.ORDER_NEW);
-            return null;
+            lastOrderID++;
+            OrderData order = OrderData.Create(lastOrderID, customID);
+            order.startTime = Time.realtimeSinceStartup;
+
+            if (!m_datas.TryAdd(order.id, order))
+            {
+                log.Error("order id add fail=" + order.id);
+                return null;
+            }
+            
+            return order;
+        }
+
+        /// <summary>
+        /// 关闭订单
+        /// </summary>
+        /// <param name="orderID"></param>
+        /// <returns></returns>
+        public bool CloseOrder(int orderID)
+        {
+            var order = Get(orderID);
+            if (order == null)
+            {
+                log.Error("order not found =" + orderID);
+                return false;
+            }
+            if (order.progress != ORDER_PROGRESS.FINISH)
+            {
+                log.Warn("order is not finish id=" + orderID);
+            }
+            
+            m_datas.Remove(orderID);
+            return true;
         }
 
         /// <summary>
@@ -58,27 +123,6 @@ namespace SGame
             }
 
             return null;
-        }
-
-        /// <summary>
-        /// 开始制作食物
-        /// </summary>
-        /// <param name="orderID"></param>
-        /// <param name="cookerID"></param>
-        public bool StartCook(int orderID, int cookerID)
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// 完成食物的制作
-        /// </summary>
-        /// <param name="orderID"></param>
-        /// <param name="cookerID"></param>
-        /// <returns></returns>
-        public bool FinishCook(int orderID, int cookerID)
-        {
-            return false;
         }
     }
 }
