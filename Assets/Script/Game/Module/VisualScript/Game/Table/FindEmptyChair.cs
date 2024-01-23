bdcc38cb43d3b875469ce71765126d5fd3c6d217
silekey <46124608+silekey2@users.noreply.github.com>
2024-01-23 18:41:01 +0800

    AI-add-添加AI相关接口

---
+++ Assets/Script/Game/Module/VisualScript/Game/Table/FindEmptyChair.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using SGame.UI;
+using Unity.Entities;
+
+namespace SGame.VS
+{
+    // 查找空的顾客位置
+    [UnitTitle("FindEmptyChair")] 
+    [UnitCategory("Game/Table")]
+    public class FindEmptyChair : Unit
+    {
+        [DoNotSerialize]
+        public ControlInput inputTrigger;
+
+        // 成功返回流程
+        [DoNotSerialize]
+        public ControlOutput outputSuccess;
+        
+        // 失败返回流程
+        [DoNotSerialize]
+        public ControlOutput outputFail;
+        
+        [DoNotSerialize]
+        public ValueInput tableType; // 桌子类型
+        
+        [DoNotSerialize]
+        public ValueInput chairType; // 座椅类型
+
+        [DoNotSerialize]
+        public ValueOutput result;
+
+        private ChairData  resultValue;
+        
+        // 端口定义
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("Input", (flow) =>
+            {
+                var table_type = flow.GetValue<TABLE_TYPE>(tableType);
+                var chair_type = flow.GetValue<CHAIR_TYPE>(chairType);
+
+                resultValue = TableManager.Instance.FindEmptyChair(table_type, chair_type);
+
+                if (resultValue == ChairData.Empty)
+                    return outputFail;
+                
+                return outputSuccess;
+            });
+
+            tableType       = ValueInput<TABLE_TYPE>("tableType", TABLE_TYPE.CUSTOM);
+            chairType       = ValueInput<CHAIR_TYPE>("chairType", CHAIR_TYPE.CUSTOMER);
+            outputSuccess   = ControlOutput("Success");
+            outputFail      = ControlOutput("Fail");
+            result          = ValueOutput<ChairData>("Chair", (flow) => resultValue);
+        }
+    }
+}
---
+++ Assets/Script/Game/Module/VisualScript/Game/Table/SitChair.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using SGame.UI;
+using Unity.Entities;
+
+namespace SGame.VS
+{
+    // 坐下
+    [UnitTitle("SitChair")] 
+    [UnitCategory("Game/Table")]
+    public class SitChair : Unit
+    {
+        [DoNotSerialize]
+        public ControlInput inputTrigger;
+
+        // 成功返回流程
+        [DoNotSerialize]
+        public ControlOutput    output;
+        
+        [DoNotSerialize]
+        public ValueOutput      result;
+        
+        [DoNotSerialize]
+        public ValueInput       _chair;
+        
+        [DoNotSerialize]
+        public ValueInput       _customID;
+
+        private bool       resultValue;
+        
+        // 端口定义
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("Input", (flow) =>
+            {
+                //Making the resultValue equal to the input value from myValueA concatenating it with myValueB.
+                var chair = flow.GetValue<ChairData>(_chair);
+                var customID = flow.GetValue<int>(_customID);
+                
+                resultValue = TableManager.Instance.SitChair(chair, customID);
+                return output;
+            });
+            
+            _chair = ValueInput<ChairData>("chair", ChairData.Empty);
+            _customID = ValueInput<int>("custom", 0);
+            output = ControlOutput("Output");
+            result = ValueOutput<bool>("success", (flow) => resultValue);
+        }
+    }
+}
