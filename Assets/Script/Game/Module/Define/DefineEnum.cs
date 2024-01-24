337c7294395596fdbc61b0c377c41e8194992552
zengchangfu <zengchangfu@uhigame.com>
2024-01-24 14:48:05 +0800

    加入静态常态变量定义位置

---
+++ Assets/Script/Game/Module/Define/DefineEnum.cs
@@ -0,0 +1,16 @@
+﻿//枚举定义
+namespace SGame
+{
+	/// <summary>
+	/// 星星枚举
+	/// </summary>
+	public enum EnumStar
+	{
+		None	= 0,
+		Green	= 1,
+		Blue	= 2,
+		Purple	= 3,
+		Orange	= 4,
+	}
+
+}
---
+++ Assets/Script/Game/Module/Define/StaticConst.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGame
+{
+	/// <summary>
+	/// 常量定义
+	/// </summary>
+	public static partial class ConstDefine
+	{
+
+	} 
+
+	/// <summary>
+	/// 静态变量定义
+	/// </summary>
+	public static partial class StaticDefine
+	{
+
+	}
+}
