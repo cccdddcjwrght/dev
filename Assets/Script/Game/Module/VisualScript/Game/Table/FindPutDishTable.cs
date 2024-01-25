340291da67ca836f20d24a92918fd8de01c78654
silekey <46124608+silekey2@users.noreply.github.com>
2024-01-25 11:59:11 +0800

    AI-add-完成厨师AI制作

--- Assets/Script/Game/Module/VisualScript/Game/Table/FindPutDishTable.cs
+++ Assets/Script/Game/Module/VisualScript/Game/Table/FindPutDishTable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FlatBuffers;
 using UnityEngine;
 using Unity.VisualScripting;
 using SGame.UI;
@@ -8,7 +9,7 @@ using Unity.Serialization;
 
 namespace SGame.VS
 {
-    // 查找空的放餐桌
+    // 查找空的放餐桌 位置
     [UnitTitle("FindPutDishTable")] 
     [UnitCategory("Game/Table")]
     public class FindPutDishTable : Unit
@@ -26,16 +27,14 @@ namespace SGame.VS
         
         
         [DoNotSerialize]
+        public ValueOutput      resultChair;
+
+        [DoNotSerialize]
         public ValueOutput      resultTable;
         
         [DoNotSerialize]
-        public ValueOutput      resultID;
+        private ChairData       _valueChair;
 
-        [DoNotSerialize]
-        private TableData       _valueTable;
-
-        
-        // 端口定义
         protected override void Definition()
         {
             inputTrigger = ControlInput("Input", (flow) =>
@@ -44,17 +43,23 @@ namespace SGame.VS
                 if (tableID <= 0)
                     return outputFail;
 
-                _valueTable = TableManager.Instance.Get(tableID);
-                if (_valueTable == null)
+                var table = TableManager.Instance.Get(tableID);
+                if (table == null)
                     return outputFail;
-                
+
+                int chairIndex = table.FindFirstChair(CHAIR_TYPE.ORDER);
+                if (chairIndex < 0)
+                    return outputFail;
+
+                _valueChair = table.GetChair(chairIndex);
                 return outputSuccess;
             });
 
             outputSuccess   = ControlOutput("Success");
             outputFail      = ControlOutput("Fail");
-            resultTable     = ValueOutput<TableData>("table", (flow) => _valueTable);
-            resultID        = ValueOutput<int>("tableID", (flow) => _valueTable != null ? _valueTable.id : 0);
+            
+            resultChair     = ValueOutput<ChairData>("OrderChair", (flow) => _valueChair);
+            resultTable     = ValueOutput<int>("tableID", (flow) => _valueChair.tableID);
         }
     }
 }
