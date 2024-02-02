1ae84853872610aaca5572eeda689d05b21af486
zengchangfu <zengchangfu@uhigame.com>
2024-02-02 15:48:40 +0800

    修改消耗

--- Assets/Script/Game/Module/DataCenter/Components/WorktableData.cs
+++ Assets/Script/Game/Module/DataCenter/Components/WorktableData.cs
@@ -402,7 +402,7 @@ namespace SGame
 			{
 				type = lvcfg.UpgradePrice(0);
 				id = lvcfg.UpgradePrice(1);
-				return lvcfg.UpgradePrice(2) * cfg.UpgradeRatio * 0.01f;
+				return (lvcfg.UpgradePrice(2) * cfg.UpgradeRatio * 0.01d).ToInt();
 			}
 			return 0;
 		}
