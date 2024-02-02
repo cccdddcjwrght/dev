43e2279aa329dc096eb48c75009bd8f0c69e873c
silekey <46124608+silekey2@users.noreply.github.com>
2024-02-02 18:12:55 +0800

    角色-add-添加角色动作同步

---
+++ Assets/Script/Game/Module/Character/CharacterAnimSystem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace SGame
+{
+    /// <summary>
+    /// 角色动画状态更改
+    /// </summary>
+    [UpdateInGroup(typeof(GameLogicGroup))]
+    public partial class CharacterAnimSystem : SystemBase
+    {
+        private int m_walkName = 0;
+        protected override void OnCreate()
+        {
+            m_walkName = Animator.StringToHash("walking");
+        }
+        protected override void OnUpdate()
+        {
+            // 同步移动状态
+            Entities.WithAll<CharacterSpawnSystem.CharacterInitalized>().ForEach((Entity e, Character character) =>
+            {
+                if (character.modelAnimator != null)
+                {
+                    character.modelAnimator.SetBool(m_walkName, character.isMoving);
+                }
+            }).WithoutBurst().Run();
+        }
+    }
+}
