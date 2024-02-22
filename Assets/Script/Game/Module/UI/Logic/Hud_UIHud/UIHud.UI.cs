e5fe315142f01343ff999828ec8befe154174eeb
meichen <403572540@qq.com>
2024-02-22 15:36:18 +0800

    修改hud页面的锚点

--- Assets/Script/Game/Module/UI/Logic/Hud_UIHud/UIHud.UI.cs
+++ Assets/Script/Game/Module/UI/Logic/Hud_UIHud/UIHud.UI.cs
@@ -10,33 +10,11 @@ namespace SGame.UI{
 	public partial class UIHud
 	{
 		partial void InitUI(UIContext context){
-			UIListener.ListenerIcon(m_view.m_rrr, new EventCallback1(_OnRrrClick));
-			UIListener.ListenerIcon(m_view.m_pgr, new EventCallback1(_OnPgrClick));
-			UIListener.ListenerIcon(m_view.m_tr, new EventCallback1(_OnTrClick));
-
+			
 		}
 		partial void UnInitUI(UIContext context){
-			UIListener.ListenerIcon(m_view.m_rrr, new EventCallback1(_OnRrrClick),remove:true);
-			UIListener.ListenerIcon(m_view.m_pgr, new EventCallback1(_OnPgrClick),remove:true);
-			UIListener.ListenerIcon(m_view.m_tr, new EventCallback1(_OnTrClick),remove:true);
+			
 
 		}
-		void SetFloatText_TitleText(string data)=>UIListener.SetText(m_view.m_rrr.m_title,data);
-		string GetFloatText_TitleText()=>UIListener.GetText(m_view.m_rrr.m_title);
-		void _OnRrrClick(EventContext data){
-			OnRrrClick(data);
-		}
-		partial void OnRrrClick(EventContext data);
-		void _OnPgrClick(EventContext data){
-			OnPgrClick(data);
-		}
-		partial void OnPgrClick(EventContext data);
-		void SetPgrValue(float data)=>UIListener.SetValue(m_view.m_pgr,data);
-		float GetPgrValue()=>UIListener.GetValue(m_view.m_pgr);
-		void _OnTrClick(EventContext data){
-			OnTrClick(data);
-		}
-		partial void OnTrClick(EventContext data);
-
 	}
 }
