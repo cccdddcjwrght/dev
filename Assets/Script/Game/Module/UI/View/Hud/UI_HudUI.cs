2e7574a62b641f2417a6d5655457c9112ccb7a2a
meichen <403572540@qq.com>
2024-02-22 15:36:18 +0800

    修改hud页面的锚点
    # Conflicts:
    #	Assets/BuildAsset/UI/Hud_fui.bytes

--- Assets/Script/Game/Module/UI/View/Hud/UI_HudUI.cs
+++ Assets/Script/Game/Module/UI/View/Hud/UI_HudUI.cs
@@ -7,9 +7,6 @@ namespace SGame.UI.Hud
 {
     public partial class UI_HudUI : GComponent
     {
-        public UI_FloatText m_rrr;
-        public UI_Progress m_pgr;
-        public UI_OrderTip m_tr;
         public const string URL = "ui://clbwsjawjx7g0";
 
         public static UI_HudUI CreateInstance()
@@ -21,9 +18,6 @@ namespace SGame.UI.Hud
         {
             base.ConstructFromXML(xml);
 
-            m_rrr = (UI_FloatText)GetChildAt(0);
-            m_pgr = (UI_Progress)GetChildAt(1);
-            m_tr = (UI_OrderTip)GetChildAt(2);
         }
     }
 }
