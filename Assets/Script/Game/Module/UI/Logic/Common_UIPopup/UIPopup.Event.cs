a9c7e6034f4a11ad8969d9ae09cd0fd1bf8957c9
silekey <46124608+silekey2@users.noreply.github.com>
2024-02-23 18:31:26 +0800

    UI-fix-解决popup不关闭问题

--- Assets/Script/Game/Module/UI/Logic/Common_UIPopup/UIPopup.Event.cs
+++ Assets/Script/Game/Module/UI/Logic/Common_UIPopup/UIPopup.Event.cs
@@ -11,14 +11,17 @@ namespace SGame.UI{
 		partial void InitEvent(UIContext context){
 			var closeBtn = m_view.m_close;
 			closeBtn.onClick.Add(OncloseBtnClick);
-			
+			context.window.AddEventListener("OnMaskClick", OnMaskClick);
 		}
-
 	
 		void OncloseBtnClick(EventContext context)
 		{
-			m_view.visible = false;
+			SGame.UIUtils.CloseUIByID(__id);
 		}
 		
+		void OnMaskClick()
+		{
+			SGame.UIUtils.CloseUIByID(__id);
+		}
 	}
 }
