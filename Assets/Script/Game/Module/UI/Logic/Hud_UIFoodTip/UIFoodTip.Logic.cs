66cff9a300afa6059fdb6da7740368ce02cf2f5e
silekey <46124608+silekey2@users.noreply.github.com>
2024-03-01 21:07:02 +0800

    添加小费金币提示

---
+++ Assets/Script/Game/Module/UI/Logic/Hud_UIFoodTip/UIFoodTip.Logic.cs
@@ -0,0 +1,41 @@
+﻿
+using Unity.Entities;
+namespace SGame.UI{
+	using FairyGUI;
+	using UnityEngine;
+	using SGame;
+	using SGame.UI.Hud;
+	
+	public partial class UIFoodTip
+	{
+		private Entity _foodTipEntity;
+		private double _value;
+		partial void InitLogic(UIContext context)
+		{
+			var param = context.gameWorld.GetEntityManager().GetComponentData<UIParam>(context.entity);
+			_foodTipEntity = (Entity)param.Value;
+			//m_view.m_title.text = Utils.ConvertNumberStr(value);
+			_value = 0;
+			context.onUpdate += OnUpdate;
+		}
+
+		void OnUpdate(UIContext context)
+		{
+			var entityManager = context.gameWorld.GetEntityManager();
+			if (entityManager.Exists(_foodTipEntity) && entityManager.HasComponent<FoodTips>(_foodTipEntity))
+			{
+				var foodTip = entityManager.GetComponentData<FoodTips>(_foodTipEntity);
+				if (_value != foodTip.gold)
+				{
+					_value = foodTip.gold;
+					m_view.m_title.text = Utils.ConvertNumberStr(_value);
+				}
+			}
+		}
+		
+		
+		partial void UnInitLogic(UIContext context){
+
+		}
+	}
+}
