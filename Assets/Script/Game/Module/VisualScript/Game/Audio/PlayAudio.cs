a6c660545e4b2e74fea6130bc06c1e1403a90bdc
silekey <46124608+silekey2@users.noreply.github.com>
2024-03-05 14:30:24 +0800

    添加-audio声音

---
+++ Assets/Script/Game/Module/VisualScript/Game/Audio/PlayAudio.cs
@@ -0,0 +1,56 @@
+using log4net;
+using SGame;
+using Unity.VisualScripting;
+using Unity.Mathematics;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace SGame.VS
+{
+    /// <summary>
+    /// 播放声音
+    /// </summary>
+	
+	[UnitTitle("PlayAudio")] //The Custom Scripting Event node to receive the Event. Add "On" to the node title as an Event naming convention.
+    [UnitCategory("Game/Audio")]
+    public sealed class PlayAudio : Unit
+    {
+        private static ILog log = LogManager.GetLogger("game.audio");
+        
+        [DoNotSerialize]
+        public ControlInput     inputTrigger;
+
+        // 成功返回流程
+        [DoNotSerialize]
+        public ControlOutput    outputTrigger;
+
+        /// <summary>
+        /// 声音ID
+        /// </summary>
+        [DoNotSerialize]
+        public ValueInput AudioID { get; private set; }
+        
+        /// <summary>
+        /// 3D位置信息
+        /// </summary>
+        [DoNotSerialize]
+        public ValueInput mapPos { get; private set; }
+
+        
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("Input", (flow) =>
+            {
+                var id =  flow.GetValue<int>(AudioID);
+                var pos = flow.GetValue<string>(mapPos);
+
+                AudioSystem.Instance.Play(id);
+                return outputTrigger;
+            });
+            
+            AudioID      = ValueInput<int>("roleID", 0);
+            mapPos      = ValueInput<Vector3>("mapPos",Vector3.zero);
+            outputTrigger = ControlOutput("Output");
+        }
+    }
+}
