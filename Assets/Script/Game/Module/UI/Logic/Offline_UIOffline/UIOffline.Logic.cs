50ba0d4dbb7de62bdbbd61c10618fc3f994011c9
zengchangfu <zengchangfu@uhigame.com>
2024-03-07 19:44:56 +0800

    离线收益-add-调整最低收益

--- Assets/Script/Game/Module/UI/Logic/Offline_UIOffline/UIOffline.Logic.cs
+++ Assets/Script/Game/Module/UI/Logic/Offline_UIOffline/UIOffline.Logic.cs
@@ -26,10 +26,13 @@ namespace SGame.UI
 			m_view.m_progress.value = val;
 			m_view.m_progress.SetText(Utils.FormatTime((int)val, needsec: false) + "/" + Utils.FormatTime((int)max, needsec: false));
 
-			var ws = DataCenter.MachineUtil.GetWorktables((w) => !w.isTable );
+			var ws = DataCenter.MachineUtil.GetWorktables((w) => !w.isTable && w.level > 0);
 
 			var gold = 0d;
-			ws.ForEach(w => gold += w.level>0 ? w.GetPrice() / w.GetWorkTime() : min);
+			if (ws?.Count > 0)
+				ws.ForEach(w => gold += w.GetPrice() / w.GetWorkTime());
+			else
+				gold = min;
 			gold = (ConstDefine.C_PER_SCALE * gold * val * rate).ToInt();
 
 			m_view.m_count.SetText(Utils.ConvertNumberStr(gold), false);
@@ -48,8 +51,10 @@ namespace SGame.UI
 		{
 			if (_isAd)
 			{
-				Utils.PlayAd(c_ad_name, (s,t) => {
-					if (s) {
+				Utils.PlayAd(c_ad_name, (s, t) =>
+				{
+					if (s)
+					{
 						_isAd = false;
 						OnClickClick(data);
 					}
