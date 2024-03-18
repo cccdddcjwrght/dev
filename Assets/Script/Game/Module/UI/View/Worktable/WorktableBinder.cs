c730fe07f4e1bd95b1be3e7447cb7a22dfe1d0d6
zengchangfu <zengchangfu@uhigame.com>
2024-03-18 12:51:09 +0800

    ui-fix-调整界面导出

--- Assets/Script/Game/Module/UI/View/Worktable/WorktableBinder.cs
+++ Assets/Script/Game/Module/UI/View/Worktable/WorktableBinder.cs
@@ -10,11 +10,8 @@ namespace SGame.UI.Worktable
         {
             UIObjectFactory.SetPackageItemExtension(UI_WorktableUI.URL, typeof(UI_WorktableUI));
             UIObjectFactory.SetPackageItemExtension(UI_Icon.URL, typeof(UI_Icon));
-            UIObjectFactory.SetPackageItemExtension(UI_Progress.URL, typeof(UI_Progress));
-            UIObjectFactory.SetPackageItemExtension(UI_Label.URL, typeof(UI_Label));
             UIObjectFactory.SetPackageItemExtension(UI_Tips.URL, typeof(UI_Tips));
             UIObjectFactory.SetPackageItemExtension(UI_WorktablePanelUI.URL, typeof(UI_WorktablePanelUI));
-            UIObjectFactory.SetPackageItemExtension(UI_Label2.URL, typeof(UI_Label2));
         }
     }
 }
