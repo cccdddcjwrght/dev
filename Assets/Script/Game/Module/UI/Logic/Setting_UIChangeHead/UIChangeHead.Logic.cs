f2233436d6fd59b32456b2903275fc062ca4530d
meichen <403572540@qq.com>
2024-03-20 17:25:59 +0800

    功能设置-头像添加点击标识

--- Assets/Script/Game/Module/UI/Logic/Setting_UIChangeHead/UIChangeHead.Logic.cs
+++ Assets/Script/Game/Module/UI/Logic/Setting_UIChangeHead/UIChangeHead.Logic.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using SGame.UI.Common;
 
 namespace SGame.UI{
@@ -12,7 +13,8 @@ namespace SGame.UI{
 		private AccountData _accountData;
 		private SetData     _setData;
 		private GList list;
-	
+		private List<SetData.HeadFrameData> headFramelist;
+
 
 		partial void BeforeInit(UIContext context)
 		{
@@ -25,9 +27,9 @@ namespace SGame.UI{
 		{
 			m_view.m_State.selectedIndex = 0;
 			list=m_view.m_list;
-			list.RemoveChildrenToPool();
-			SGame.UIUtils.AddListItems(list, _setData.headDataList, OnSetHeadList);
-		
+			headFramelist = _setData.headDataList;
+			list.itemRenderer = OnSetHeadList;
+			list.numItems = headFramelist.Count;
 		}
 		
 
@@ -35,16 +37,15 @@ namespace SGame.UI{
 		{
 			if (m_view.m_State.selectedIndex == 0)
 			{
-				list.RemoveChildrenToPool();
-				SGame.UIUtils.AddListItems(list, _setData.headDataList, OnSetHeadList);
+				headFramelist = _setData.headDataList;
+				
 				
 			}
 			else
 			{
-				list.RemoveChildrenToPool();
-				SGame.UIUtils.AddListItems(list, _setData.freamDataList, OnSetHeadList);
+				headFramelist = _setData.freamDataList;
 			}
-			
+			list.numItems = headFramelist.Count;
 		}
 		
          /// <summary>
@@ -52,13 +53,13 @@ namespace SGame.UI{
          /// </summary>
          /// <param name="index"></param>
          /// <param name="item"></param>
-		private void OnSetHeadList(int index,object data, GObject item)
+		private void OnSetHeadList(int index, GObject item)
          {
-	        var headData= data as SetData.HeadFrameData;
+	        var headData= headFramelist[index];
 	        var obj = item as UI_SimpleHeadIcon;
-	        
 	        item.onClick.Set(()=>
 	        {
+		        
 		        var showHead = m_view.m_icon as UI_HeadBtn;
 		        if (m_view.m_State.selectedIndex == 0)
 		        {
@@ -70,6 +71,7 @@ namespace SGame.UI{
 			        showHead.m_frame.url=string.Format("ui://IconHead/{0}",headData.icon);
 			        _accountData.frame = headData.id;
 		        }
+		        list.numItems = headFramelist.Count;
 		        EventManager.Instance.Trigger(((int)GameEvent.SETTING_UPDATE_HEAD));
 	        });
 	        
@@ -85,15 +87,15 @@ namespace SGame.UI{
 				var head = obj.m_body as UI_HeadBtn;
 				head.m_frame.url=string.Format("ui://IconHead/{0}",headData.icon);
 			}
-			//
-			// if (_accountData.head == headData.id || _accountData.frame == headData.id)
-			// {
-			// 	obj.m_check.selectedIndex = 1;
-			// }
-			// else
-			// {
-			// 	obj.m_check.selectedIndex = 0;
-			// }
+			
+			if (_accountData.head == headData.id || _accountData.frame == headData.id)
+			{
+				obj.m_check.selectedIndex = 1;
+			}
+			else
+			{
+				obj.m_check.selectedIndex = 0;
+			}
          }
 
 		partial void UnInitLogic(UIContext context){
