8ab5e3100ea4c10b5807cdfd34bd4d4dee6e82a0
xiawei <eijw@163.com>
2024-04-08 17:40:56 +0800

    调整好友ui

---
+++ Assets/BuildAsset/UI/GameFriend/GameFriendBinder.cs
@@ -0,0 +1,19 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace GameFriend
+{
+    public class GameFriendBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(UI_Comfirm.URL, typeof(UI_Comfirm));
+            UIObjectFactory.SetPackageItemExtension(UI_FriendUI.URL, typeof(UI_FriendUI));
+            UIObjectFactory.SetPackageItemExtension(UI_MyRole.URL, typeof(UI_MyRole));
+            UIObjectFactory.SetPackageItemExtension(UI_FriendItem.URL, typeof(UI_FriendItem));
+            UIObjectFactory.SetPackageItemExtension(UI_equip.URL, typeof(UI_equip));
+            UIObjectFactory.SetPackageItemExtension(UI_FriendDetailUI.URL, typeof(UI_FriendDetailUI));
+        }
+    }
+}
