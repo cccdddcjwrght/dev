0921f6cea53cab23d091f609bbd17541d9de72c8
silekey <46124608+silekey2@users.noreply.github.com>
2024-04-10 14:49:38 +0800

    好友-fix-UI调整

--- Assets/Script/Game/Module/UI/Logic/GameFriend_UIFriend/UIFriend.Event.cs
+++ Assets/Script/Game/Module/UI/Logic/GameFriend_UIFriend/UIFriend.Event.cs
@@ -58,7 +58,7 @@ namespace SGame.UI{
 			else
 			{
 				m_view.m_empty.selectedIndex = 1;
-				m_view.m_friendGroup.y = 320;
+				m_view.m_friendGroup.y = 315;
 			}
 			
 			m_view.m_listFirends.numItems = friends.Friends.Count;
