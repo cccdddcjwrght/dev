6494790e134be27119c17dbe024be40b94f76a92
silekey <46124608+silekey2@users.noreply.github.com>
2024-04-11 16:39:38 +0800

    新手礼品-add-完成新手礼品基本逻辑

--- Assets/Script/Game/Module/UI/Logic/TomorrowGift_UINewbieGift/UINewbieGift.Event.cs
+++ Assets/Script/Game/Module/UI/Logic/TomorrowGift_UINewbieGift/UINewbieGift.Event.cs
@@ -4,14 +4,56 @@ namespace SGame.UI{
 	using UnityEngine;
 	using SGame;
 	using SGame.UI.TomorrowGift;
+	using SGame;
+	using SGame.UI.Shop;
+	using GameConfigs;
 	
 	public partial class UINewbieGift
 	{
-		partial void InitEvent(UIContext context){
+		UI_Probability probabilityUI
+		{
+			get { return m_view.m_probablity as UI_Probability; }
+		}
 
+		
+		partial void InitEvent(UIContext context){
+			if (!ConfigSystem.Instance.TryGet(NewbieGiftModule.GOOD_ITEM_ID, out ShopRowData config))
+			{
+				log.Error("goold id not found=" + NewbieGiftModule.GOOD_ITEM_ID);
+				return;
+			}
+
+			probabilityUI.m_bg.onClick.Add(OnRateClose);
+			probabilityUI.m_bg.GetChild("close").onClick.Add(OnRateClose);
+			m_view.m_btnOK.onClick.Add(OnClickTakeGift);
+
+			m_view.m_btnOK.text = LanagueSystem.Instance.GetValue(config.ShopName);
+			
+			m_view.m_gift.Initalize(NewbieGiftModule.GOOD_ITEM_ID, OnRateOpen);
 		}
 		partial void UnInitEvent(UIContext context){
 
+		}
+		
+		void OnRateOpen()
+		{
+			ConfigSystem.Instance.TryGet(NewbieGiftModule.GOOD_ITEM_ID, out ShopRowData config);
+			probabilityUI.SetRates(config.GetChestInfoArray());
+			probabilityUI.m_show.selectedIndex = 1;
+		}
+
+		void OnClickTakeGift()
+		{
+			if (NewbieGiftModule.Instance.TakeGift())
+			{
+				SGame.UIUtils.CloseUIByID(__id);
+				EventManager.Instance.Trigger((int)GameEvent.GAME_MAIN_REFRESH);
+			}
+		}
+		
+		void OnRateClose()
+		{
+			probabilityUI.m_show.selectedIndex = 0;
 		}
 	}
 }
--- Assets/Script/Game/Module/UI/Logic/TomorrowGift_UINewbieGift/UINewbieGift.Logic.cs
+++ Assets/Script/Game/Module/UI/Logic/TomorrowGift_UINewbieGift/UINewbieGift.Logic.cs
@@ -3,27 +3,20 @@ namespace SGame.UI{
 	using FairyGUI;
 	using UnityEngine;
 	using SGame;
-	using SGame.UI.TomorrowGift;
 	using SGame.UI.Shop;
+	using GameConfigs;
 	
 	public partial class UINewbieGift
 	{
-		UI_Probability probabilityUI
-		{
-			get { return m_view.m_probablity as UI_Probability; }
-		}
-		
+
 		partial void InitLogic(UIContext context){
-			probabilityUI.m_bg.onClick.Add(OnRateClose);
-			probabilityUI.m_bg.GetChild("close").onClick.Add(OnRateClose);
+
+			
 		}
 		partial void UnInitLogic(UIContext context){
 
 		}
-		
-		void OnRateClose()
-		{
-			probabilityUI.m_show.selectedIndex = 0;
-		}
+
+
 	}
 }
