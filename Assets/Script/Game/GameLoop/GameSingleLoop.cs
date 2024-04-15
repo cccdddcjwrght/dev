d4fff4fd7015045b547ff7961a1aac1c4469aba5
silekey <46124608+silekey2@users.noreply.github.com>
2024-04-15 10:35:11 +0800

    成长礼包-add-初始化配置

--- Assets/Script/Game/GameLoop/GameSingleLoop.cs
+++ Assets/Script/Game/GameLoop/GameSingleLoop.cs
@@ -66,7 +66,8 @@ namespace SGame
 			
 			NewbieGiftModule.Instance.Initalize();
 
-			PiggyBankModule.Instance.Initalize();
+PiggyBankModule.Instance.Initalize();
+			GrowGiftModule.Instance.Initialize();
 		}
 
 		/// <summary>
