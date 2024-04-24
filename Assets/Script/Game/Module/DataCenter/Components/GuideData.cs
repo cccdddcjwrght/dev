092bb9014760477e58729ea77df4d7f310b418f2
chenjiawang <chenjiawang@crowd-star.com>
2024-04-24 10:07:33 +0800

    新手-add-记录数据

--- Assets/Script/Game/Module/DataCenter/Components/GuideData.cs
+++ Assets/Script/Game/Module/DataCenter/Components/GuideData.cs
@@ -10,5 +10,11 @@ namespace SGame
         [NonSerialized]
         public bool isGuide=false;
     }
+
+    public class GuideModule :Singleton<GuideModule>
+    {
+        //是否第一次进入指引
+        public const string GUIDE_FIRST = "guide.first";
+    }
 }
 
