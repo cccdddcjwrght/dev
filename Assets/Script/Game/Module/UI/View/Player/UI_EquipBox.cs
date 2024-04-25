7b96593cd93e86ff26bf017096663957e1f8db49
lantian <zouchunhui@uhigame.com>
2024-04-25 22:20:57 +0800

    fix

--- Assets/Script/Game/Module/UI/View/Player/UI_EquipBox.cs
+++ Assets/Script/Game/Module/UI/View/Player/UI_EquipBox.cs
@@ -9,7 +9,6 @@ namespace SGame.UI.Player
     {
         public Controller m_quality;
         public Controller m___redpoint;
-        public GGraph m_size;
         public UI_Equip m_body;
         public Transition m_show;
         public const string URL = "ui://cmw7t1elw46k1e";
@@ -25,8 +24,7 @@ namespace SGame.UI.Player
 
             m_quality = GetControllerAt(0);
             m___redpoint = GetControllerAt(1);
-            m_size = (GGraph)GetChildAt(0);
-            m_body = (UI_Equip)GetChildAt(1);
+            m_body = (UI_Equip)GetChildAt(0);
             m_show = GetTransitionAt(0);
         }
     }
