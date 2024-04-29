a61d83a71bdb7e4b9cfc2b90dae397e97933b7ff
zengchangfu <zengchangfu@uhigame.com>
2024-04-29 17:09:33 +0800

    sdk-add-超时检测网络

--- Assets/Script/Game/Module/Ad/AdExt.cs
+++ Assets/Script/Game/Module/Ad/AdExt.cs
@@ -1,4 +1,4 @@
-#if USE_AD
+#if USE_THIRD_SDK
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -42,17 +42,23 @@ namespace SGame
 				var flag = false;
 				var c = default(Action<bool>);
 				if (type != EnumAD.Banner)
-					c = AdWait(() => flag, key);
+					c = AdWait(() => flag, key, c_ad_timeout);
 
 				UILockManager.Instance.Require(key);
 				THSdk.Instance.PlayAd(type, key, (s) =>
 				{
-					Debug.Log("[ad]end!!!");
-					complete?.Invoke(s, null);
 					UILockManager.Instance.Release(key);
-
-					if (type != EnumAD.Banner)
-					{ flag = true; c?.Invoke(false); }
+					c?.Invoke(true);
+					flag = true;
+					Debug.Log("[ad]end!!!");
+					try
+					{
+						complete?.Invoke(s, null);
+					}
+					catch (Exception e)
+					{
+						log.Error(e.Message + "-" + e.StackTrace);
+					}
 				}, timeout: c_ad_timeout);
 			}
 		}
@@ -62,19 +68,27 @@ namespace SGame
 			state = THSdk.Instance.IsAdLoaded(ad);
 		}
 
-		static Action<bool> AdWait(Func<bool> cacncel, string key = null)
+		static Action<bool> AdWait(Func<bool> cacncel, string key = null, float wait = 0)
 		{
-			var time = 0f;
+			var time = 1f;
+			wait = wait > 0 ? wait : 10;
 			Action<bool> timer = null;
-			timer = Timer(60, () =>
+			timer = Timer(wait, () =>
 			{
-				if (cacncel()) timer?.Invoke(false);
+				//Õ¯¬Á“Ï≥£
+				if (Application.internetReachability == NetworkReachability.NotReachable)
+				{
+					timer.Invoke(obj: true);
+					"@ui_net_error".Tips();
+				}
+				else if (cacncel())
+					timer?.Invoke(true);
 				else if ((time -= Time.deltaTime) < 0)
 				{
-					time = 5f;
+					time = wait * 0.5f;
 					"@ui_ad_waiting".Tips();
 				}
-			}, key, completed: () => UILockManager.Instance.Release(key), delay: 2f);
+			}, key, completed: () => UILockManager.Instance.Release(key));
 
 			return timer;
 		}
