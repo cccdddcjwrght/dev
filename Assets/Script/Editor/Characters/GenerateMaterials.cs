121b7efdd42fcc73aed957f3e90e95cb3b7d31e2
silekey <46124608+silekey2@users.noreply.github.com>
2024-05-30 15:51:41 +0800

    性能优化-fix-角色材质不使用pbr

--- Assets/Script/Editor/Characters/GenerateMaterials.cs
+++ Assets/Script/Editor/Characters/GenerateMaterials.cs
@@ -64,7 +64,8 @@ class GenerateMaterials
                     }
 
                     // Use a default shader according to artist preferences.
-                    string shader = "Universal Render Pipeline/Unlit";
+                    //string shader = "Universal Render Pipeline/Unlit";
+                    string shader = "Universal Render Pipeline/Simple Lit";
                     //if (normalmap != null) shader = "Universal Render Pipeline/Lit";
                     //string shader = "Universal Render Pipeline/Lit";
 
