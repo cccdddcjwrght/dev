9641abcd95b5d4093c05953d75cd5973bd9095a3
silekey <46124608+silekey2@users.noreply.github.com>
2024-06-07 23:17:10 +0800

    食谱-task 1361 添加遗漏代码

---
+++ Assets/Script/Game/Module/UI/View/Cookbook/UI_PropertyUpdateIcon.cs
@@ -0,0 +1,31 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+using FairyGUI.Utils;
+
+namespace SGame.UI.Cookbook
+{
+    public partial class UI_PropertyUpdateIcon : GLabel
+    {
+        public Controller m_state;
+        public GTextField m_val2;
+        public GTextField m_val;
+        public GTextField m_next;
+        public const string URL = "ui://n2tgmsyuq90gc";
+
+        public static UI_PropertyUpdateIcon CreateInstance()
+        {
+            return (UI_PropertyUpdateIcon)UIPackage.CreateObject("Cookbook", "PropertyUpdateIcon");
+        }
+
+        public override void ConstructFromXML(XML xml)
+        {
+            base.ConstructFromXML(xml);
+
+            m_state = GetControllerAt(0);
+            m_val2 = (GTextField)GetChildAt(1);
+            m_val = (GTextField)GetChildAt(2);
+            m_next = (GTextField)GetChildAt(4);
+        }
+    }
+}
