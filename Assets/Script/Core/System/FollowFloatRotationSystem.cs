c8c41b981770918ce11fb2c715daef307d1f724b
silekey <46124608+silekey2@users.noreply.github.com>
2024-06-13 15:59:26 +0800

    基础功能-add-添加3D路径功能

---
+++ Assets/Script/Core/System/FollowFloatRotationSystem.cs
@@ -0,0 +1,94 @@
+using GameTools.Paths;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace SGame
+{
+    // 使用 PathPositions 的版本
+    //[UpdateInGroup(typeof(GameLogicGroup))]
+    public partial class FollowFloatRotationSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var mapInfo = AStar.map.GetMapInfo();
+            float deltaTime = Time.DeltaTime;
+            Entities.WithNone<LastRotation>().ForEach(
+                (   Entity                       e, 
+                ref Rotation                     rot, 
+                in  DynamicBuffer<FPathPositions> paths, 
+                in Follow follow,
+                in Translation translation,
+                in RotationSpeed speed) =>
+            {
+                if (follow.Value <= 0)
+                {
+                    return;
+                }
+
+                int targetIndex     = follow.Value - 1;
+                var map_pos     = paths[targetIndex].Value;
+                float3 target_pos = map_pos;//AStar.GetPos(map_pos.x, map_pos.y, mapInfo);
+                float3 dir = target_pos - translation.Value;
+                dir.y = 0;
+                if (math.lengthsq(dir) <= 0.01) // 距离太短, 不足以判定
+                    return;
+
+                dir = math.normalize(dir);
+                quaternion target_rot = quaternion.LookRotation(dir, new float3(0, 1, 0));
+
+                float cos_value = math.dot(rot.Value, target_rot);
+                if (cos_value >= 0.99998f)
+                {
+                    rot.Value = target_rot;
+                }
+                else
+                {
+                    float t = math.clamp(deltaTime * speed.Value, 0, 1.0f);
+                    rot.Value = math.nlerp(rot.Value, target_rot, t);;
+                }
+            }).WithBurst().ScheduleParallel();
+            
+            Entities.ForEach(
+                (   Entity                       e, 
+                    ref Rotation                     rot, 
+                    in  DynamicBuffer<FPathPositions> paths, 
+                    in Follow follow,
+                    in Translation translation,
+                    in RotationSpeed speed,
+                    in LastRotation lookAt) =>
+                {
+                    quaternion target_rot = quaternion.identity;
+                    if (follow.Value <= 0)
+                    {
+                        target_rot = lookAt.Value;
+                    }
+                    else
+                    {
+                        int targetIndex     = follow.Value - 1;
+                        var map_pos    = paths[targetIndex].Value;
+                        float3 target_pos   = map_pos;
+                        float3 dir = target_pos - translation.Value;
+                        dir.y = 0;
+                        if (math.lengthsq(dir) <= 0.01) // 距离太短, 不足以判定
+                            return;
+                        
+                        dir = math.normalize(dir);
+                        target_rot = quaternion.LookRotation(dir, new float3(0, 1, 0));
+                    }
+
+                    float cos_value = math.dot(rot.Value, target_rot);
+                    if (cos_value >= 0.99998f)
+                    {
+                        rot.Value = target_rot;
+                    }
+                    else
+                    {
+                        float t = math.clamp(deltaTime * speed.Value, 0, 1.0f);
+                        rot.Value = math.nlerp(rot.Value, target_rot, t);;
+                    }
+                }).WithBurst().ScheduleParallel();
+        }
+    }
+}
---
+++ Assets/Script/Core/System/FollowFloatSystem.cs
@@ -0,0 +1,124 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Burst;
+
+//using UnityEngine;
+// 自动寻路的跟随系统 (多线程加速版, 使用的是浮点位置)
+namespace SGame
+{
+	public struct FPathPositions : IBufferElementData
+	{
+		public float3 Value;
+	}
+	
+	public partial class FollowFloatSystem : SystemBase
+	{
+		EntityQuery m_Query;
+		EndSimulationEntityCommandBufferSystem m_CommandBuffer;
+
+		[BurstCompile]
+		struct FollowJob : IJobChunk
+		{
+			public EntityCommandBuffer.ParallelWriter _commandBuffer;
+
+
+			public float DeltaTime;
+
+			public ComponentTypeHandle<Follow> chunkTypeFollow;
+			public ComponentTypeHandle<Translation> chunkTypeTranslation;
+
+			[ReadOnly]
+			public ComponentTypeHandle<Speed> chunkTypeSpeed;
+
+			[ReadOnly]
+			public BufferTypeHandle<FPathPositions> chunkTypePathPositions;
+
+			public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+			{
+				var chunkFollow = chunk.GetNativeArray(chunkTypeFollow);
+				var chunkTranslation = chunk.GetNativeArray(chunkTypeTranslation);
+				var chunkSpeed = chunk.GetNativeArray(chunkTypeSpeed);
+				var chunkPathPositions = chunk.GetBufferAccessor(chunkTypePathPositions);
+
+				for (int i = 0; i < chunk.Count; i++)
+				{
+					Follow pathIndex = chunkFollow[i];
+					if (pathIndex.Value <= 0 )
+						continue;
+
+					Speed comSpeed = chunkSpeed[i];
+					DynamicBuffer<FPathPositions> comPositions = chunkPathPositions[i];
+					Translation translation = chunkTranslation[i];
+
+					float deltaMovement = DeltaTime * comSpeed.Value;
+					int fllowIndex = pathIndex.Value - 1;
+					var map_pos = comPositions[fllowIndex].Value;
+
+					float3 target_pos = map_pos;        
+					var currentPos = translation.Value;
+
+					var offset = (target_pos - currentPos);
+					var offsetLen = math.length(offset);
+					if (offsetLen <= deltaMovement || offsetLen <= 0.001f)
+					{
+						// Next Node
+						translation.Value = target_pos;
+						pathIndex.Value--;
+						chunkFollow[i] = pathIndex;
+					}
+					else
+					{
+						// 直接移动
+						translation.Value = currentPos + math.normalize(offset) * deltaMovement;
+					}
+					chunkTranslation[i] = translation;
+
+				}
+			}
+		}
+
+		protected override void OnCreate()
+		{
+			var query = new EntityQueryDesc()
+			{
+				All = new ComponentType[] {
+					typeof(Follow),
+					typeof(Translation),
+					ComponentType.ReadOnly<Speed>(),
+					ComponentType.ReadOnly<FPathPositions>()
+				},
+				None = new ComponentType[] {
+					typeof(FindPathParams)
+				}
+			};
+			m_Query = GetEntityQuery(query);
+			m_CommandBuffer = this.World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+		}
+
+		protected override void OnUpdate()
+		{
+			var chunkTypeFollow = GetComponentTypeHandle<Follow>();
+			var chunkTypeTranslation = GetComponentTypeHandle<Translation>();
+			var chunkTypeSpeed = GetComponentTypeHandle<Speed>(true);
+			var chunkTypePathPositions = GetBufferTypeHandle<FPathPositions>(true);
+			var commandBuffer = m_CommandBuffer.CreateCommandBuffer().AsParallelWriter();
+
+			FollowJob job = new FollowJob()
+			{
+				DeltaTime = Time.DeltaTime,
+				chunkTypeFollow = chunkTypeFollow,
+				chunkTypeTranslation = chunkTypeTranslation,
+				chunkTypeSpeed = chunkTypeSpeed,
+				chunkTypePathPositions = chunkTypePathPositions,
+				_commandBuffer = commandBuffer,
+			};
+
+			Dependency = job.ScheduleParallel(m_Query, Dependency);
+			m_CommandBuffer.AddJobHandleForProducer(Dependency);
+
+		}
+	}
+}
