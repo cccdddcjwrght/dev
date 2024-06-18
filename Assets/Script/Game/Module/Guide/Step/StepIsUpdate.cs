ae16ddce71c3d8df79ffe287694f4327ae97220a
chenjiawang <chenjiawang@crowd-star.com>
2024-06-18 16:54:04 +0800

    指引-add-添加新步骤（控制客人刷新）

---
+++ Assets/Script/Game/Module/Guide/Step/StepIsUpdate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SGame 
+{
+    /// <summary>
+    /// 是否刷新客人
+    /// </summary>
+    public class StepIsUpdate : Step
+    {
+        public override IEnumerator Excute()
+        {
+            var isUpdate = m_Config.FloatParam(0);
+            DataCenter.Instance.guideData.isStopCreate = isUpdate < 0;
+            yield break;
+        }
+    }
+}
+
