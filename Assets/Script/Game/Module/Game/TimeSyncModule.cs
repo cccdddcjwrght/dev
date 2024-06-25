d00fe8403f603bcf54441bd2c6fba041952bdf18
zengchangfu <zengchangfu@uhigame.com>
2024-06-25 19:01:38 +0800

    登录-add-获取时间加入超时

--- Assets/Script/Game/Module/Game/TimeSyncModule.cs
+++ Assets/Script/Game/Module/Game/TimeSyncModule.cs
@@ -6,27 +6,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SGame 
+namespace SGame
 {
-    public class TimeSyncModule
-    {
-        private static ILog log = LogManager.GetLogger("game.servertime");
-        public static IEnumerator GetServerTime() 
-        {
-            HttpPackage pkg = new HttpPackage();
-            var result = HttpSystem.Instance.Post("timestamp", pkg.ToJson());
-            yield return result;
+	public class TimeSyncModule
+	{
+		private static ILog log = LogManager.GetLogger("game.servertime");
+		public static IEnumerator GetServerTime()
+		{
+			var time = 2f;//³¬Ê±
+			HttpPackage pkg = new HttpPackage();
+			var result = HttpSystem.Instance.Post("timestamp", pkg.ToJson());
+			while ((time -= Time.deltaTime) > 0 && !result.isDone)
+				yield return null;
 
-            if (!string.IsNullOrEmpty(result.error))
-            {
-                GameServerTime.Instance.Update((int)DateTimeOffset.Now.ToUnixTimeSeconds(), -1);
-                log.Warn("get serverTime data fail=" + result.error);
-                yield break;
-            }
+			if ( !result.isDone || !string.IsNullOrEmpty(result.error))
+			{
+				GameServerTime.Instance.Update((int)DateTimeOffset.Now.ToUnixTimeSeconds(), -1);
+				log.Warn("get serverTime data fail=" + result.error);
+				yield break;
+			}
 
-            pkg = JsonUtility.FromJson<HttpPackage>(result.data);
-            GameServerTime.Instance.Update(int.Parse(pkg.data), -1);
-        }
-    }
+			pkg = JsonUtility.FromJson<HttpPackage>(result.data);
+			GameServerTime.Instance.Update(int.Parse(pkg.data), -1);
+		}
+	}
 }
 
