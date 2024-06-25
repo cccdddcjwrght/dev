307c05040ca829ae44479a2c15d5a561beb8b436
silekey <46124608+silekey2@users.noreply.github.com>
2024-06-25 10:10:18 +0800

    配置-add-更新

--- Assets/Script/Game/Module/UI/View/Common/UI_BgIcon.cs
+++ Assets/Script/Game/Module/UI/View/Common/UI_BgIcon.cs
@@ -10,6 +10,7 @@ namespace SGame.UI.Common
         public Controller m_hidebg;
         public Controller m___redpoint;
         public Controller m_bgtype;
+        public Controller m_lock;
         public GLoader m_bg;
         public GLoader m_red;
         public const string URL = "ui://2w8thcm7r4i17";
@@ -26,6 +27,7 @@ namespace SGame.UI.Common
             m_hidebg = GetControllerAt(0);
             m___redpoint = GetControllerAt(1);
             m_bgtype = GetControllerAt(2);
+            m_lock = GetControllerAt(3);
             m_bg = (GLoader)GetChildAt(0);
             m_red = (GLoader)GetChildAt(2);
         }
