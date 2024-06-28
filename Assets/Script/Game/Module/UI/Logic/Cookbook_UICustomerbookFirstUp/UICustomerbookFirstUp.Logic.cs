a11f2edd465278ea0ddc74fd8b35dc975b3d2107
silekey <46124608+silekey2@users.noreply.github.com>
2024-06-28 20:37:21 +0800

    客户图鉴-add-添加弹框延迟响应0.75秒

--- Assets/Script/Game/Module/UI/Logic/Cookbook_UICustomerbookFirstUp/UICustomerbookFirstUp.Logic.cs
+++ Assets/Script/Game/Module/UI/Logic/Cookbook_UICustomerbookFirstUp/UICustomerbookFirstUp.Logic.cs
@@ -10,6 +10,10 @@ namespace SGame.UI{
 	{
 		private CustomerBookData m_data;
 		private Entity m_entity;
+		private double m_recordTime = 0;
+
+		private const double LOCK_TIME = 0.75;
+		
 		partial void InitLogic(UIContext context){
 			var mgr = World.DefaultGameObjectInjectionWorld.EntityManager;
 			m_data = mgr.GetComponentObject<UIParam>(context.entity).Value as CustomerBookData;
@@ -21,11 +25,19 @@ namespace SGame.UI{
 			m_view.m_title.text = m_data.Name;
 			m_view.m_icon.SetIcon(m_data.Icon);
 
+			m_recordTime = GlobalTime.passTime;
+
 			EffectSystem.Instance.SpawnUI(9, m_view.m___effect);
 		}
 
 		void OnMaskClick()
 		{
+			if (GlobalTime.passTime - m_recordTime < LOCK_TIME)
+			{
+				log.Info("ui is locking");
+				return;
+			}
+			
 			SGame.UIUtils.CloseUI(m_entity);
 			
 			EntityManager mgr = World.DefaultGameObjectInjectionWorld.EntityManager;
