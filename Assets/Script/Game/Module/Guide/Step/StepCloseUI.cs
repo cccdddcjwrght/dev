8a24cb417b7b499602038645d5424a186d34814f
chenjiawang <chenjiawang@crowd-star.com>
2024-07-04 09:49:30 +0800

    新手-bug-修改关闭所有界面步骤（关闭正在打开的界面）

--- Assets/Script/Game/Module/Guide/Step/StepCloseUI.cs
+++ Assets/Script/Game/Module/Guide/Step/StepCloseUI.cs
@@ -1,5 +1,7 @@
+using SGame.UI;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
 
 namespace SGame 
@@ -10,6 +12,15 @@ namespace SGame
         {
             UIUtils.CloseAllUI("mainui", "flight", "lockred", "SystemTip",
             "Redpoint", "ordertip", "progress", "FoodTip");
+
+            EntityQuery entityQuery = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(typeof(UIRequest));
+            var uiRequest = entityQuery.ToEntityArray(Unity.Collections.Allocator.Temp);
+            for (int i = 0; i < uiRequest.Length; i++)
+            {
+                var entity = uiRequest[i];
+                UIModule.Instance.CloseUI(entity);
+            }
+            uiRequest.Dispose();
 
             Finish();
             yield break;
