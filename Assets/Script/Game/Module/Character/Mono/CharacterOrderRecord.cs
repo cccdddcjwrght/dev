6705691f3c57a0b2de4e27ba4e4772b485e49b95
silekey <46124608+silekey2@users.noreply.github.com>
2024-07-11 14:13:30 +0800

    AI-add-支持获取多订单

--- Assets/Script/Game/Module/Character/Mono/CharacterOrderRecord.cs
+++ Assets/Script/Game/Module/Character/Mono/CharacterOrderRecord.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using log4net;
+using Unity.Entities;
 using UnityEngine;
 
 namespace SGame
@@ -11,40 +12,55 @@ namespace SGame
     public class CharacterOrderRecord
     {
         private static ILog log = LogManager.GetLogger("game.character");
-        private bool m_isIdle       = false;
+        //private bool m_isIdle       = false;
         private int  m_characterID  = 0;
         private int  m_takeOrderNum     = 0;  // 
+
+        /// <summary>
+        /// 任务数据
+        /// </summary>
+        public struct TaskData
+        {
+            public OrderData orderData;      // 订单数据
+            public ChairData customerChair;  // 点单的顾客位置
+            public ChairData workerChair;    // 分配的工作位置
+
+            public bool isOrder => orderData != null;
+        }
+
+        // 
+        private List<TaskData> m_datas = new List<TaskData>();
         
         /// <summary>
         /// 是否是空闲状态
         /// </summary>
-        public bool isIdle => m_isIdle;
+        public bool isIdle => m_datas.Count == 0;
 
-        public bool hasWorking => !m_isIdle && (m_orderData != null || m_customerChair != ChairData.Null);
+        public bool hasWorking => m_datas.Count > 0;//(m_orderData != null || m_customerChair != ChairData.Null);
 
         // 身上待处理的订单
-        private OrderData m_orderData = null; 
+        //private OrderData m_orderData = null; 
 
         // 待处理的角色座椅, 用于去到客户位置
-        private ChairData m_customerChair = ChairData.Null;
+        //private ChairData m_customerChair = ChairData.Null;
         
         // 工作台位置, 用于制作食物 
-        private ChairData m_workerChair = ChairData.Null;
+        //private ChairData m_workerChair = ChairData.Null;
 
-        public OrderData order => m_orderData;
+        public OrderData order => m_datas[0].orderData;
         
-        public int       orderID => m_orderData != null ? m_orderData.id : 0;
+        public int       orderID => m_datas.Count > 0 && m_datas[0].orderData != null ? m_datas[0].orderData.id : 0;
 
-        public ChairData customerChair => m_customerChair;
+        public ChairData customerChair => m_datas[0].customerChair;
         
-        public ChairData workerChair => m_workerChair;
+        public ChairData workerChair => m_datas[0].workerChair;
 
         public int takeOrderNum => m_takeOrderNum;
 
         public void Initalize(int characterID)
         {
             m_characterID = characterID;
-            m_isIdle = false;
+            m_datas.Clear();
         }
         
         /// <summary>
@@ -54,21 +70,19 @@ namespace SGame
         /// <param name="workerChair">工位</param>
         public bool AddOrder(OrderData order, ChairData workerChair)
         {
-            m_takeOrderNum = 0;
-            m_orderData = order;
             if (workerChair == ChairData.Null)
             {
                 log.Error("worker chair is null");
                 return false;
             }
+
+            AddTask(new TaskData() { orderData = order, workerChair = workerChair});
             
             // 锁定座位
             TableManager.Instance.SitChair(workerChair, m_characterID);
-            m_workerChair = workerChair;
 
             // 修改订单状态
-            m_orderData.CookerTake(m_characterID);
-            m_isIdle = false;
+            order.CookerTake(m_characterID);
             return true;
         }
 
@@ -79,10 +93,8 @@ namespace SGame
         /// <returns></returns>
         public bool AddFoodReadlyOrder(OrderData order)
         {
-            m_takeOrderNum = 0;
-            m_orderData = order;
+            AddTask(new TaskData() { orderData = order });
             order.MoveToCustom(m_characterID);
-            m_isIdle = false;
             return true;
         }
 
@@ -92,19 +104,13 @@ namespace SGame
         /// <param name="chair"></param>
         public void AddCustomerChair(ChairData chair)
         {
-            m_takeOrderNum++;
-            m_customerChair = chair;
-            m_isIdle = false;
+            AddTask(new TaskData() { orderData = null, customerChair = chair});
         }
 
-        public void WaiterTakeFoodToCustomer(ChairData chair)
-        {
-            
-        }
 
-        public bool hasOrder => m_orderData != null;
+        public bool hasOrder => m_datas.Count > 0 && m_datas[0].isOrder;//m_orderData != null;
 
-        public bool hasCustomerChair => m_customerChair != ChairData.Null;
+        public bool hasCustomerChair => m_datas.Count > 0 && !m_datas[0].isOrder; //m_customerChair != ChairData.Null;
 
         /// <summary>
         /// 进入IDLE
@@ -112,23 +118,48 @@ namespace SGame
         public void EnterIdle()
         {
             // 没有待处理的数据
-            m_orderData = null;
-            LeaveAllChairs();
-            m_isIdle = true;
+            PopTask();
+            //LeaveAllChairs();
         }
 
+        void AddTask(TaskData task)
+        {
+            if (task.isOrder)
+                m_takeOrderNum = 0;
+            else
+                m_takeOrderNum++;
+            m_datas.Add(task);
+        }
+
+        void PopTask()
+        {
+            if (m_datas.Count == 0)
+            {
+                log.Error("data is null");
+                return;
+            }
+
+            var item = m_datas[0];
+            if (item.isOrder && item.workerChair != ChairData.Null)
+            {
+                TableManager.Instance.LeaveChair(item.workerChair, m_characterID);
+            }
+            m_datas.RemoveAt(0);
+        }
+        
         /// <summary>
         /// 解锁座位
         /// </summary>
         public void LeaveAllChairs()
         {
-            if (m_workerChair != ChairData.Null)
+            var item = m_datas[0];
+            if (item.isOrder && item.workerChair != ChairData.Null)
             {
-                TableManager.Instance.LeaveChair(m_workerChair, m_characterID);
-                m_workerChair = ChairData.Null;
+                TableManager.Instance.LeaveChair(item.workerChair, m_characterID);
+                item.workerChair = ChairData.Null;
+                item.customerChair = ChairData.Null;
+                m_datas[0] = item;
             }
-            
-            m_customerChair = ChairData.Null;
         }
     }
 }
