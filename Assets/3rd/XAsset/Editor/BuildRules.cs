f72a393588ea0da57a5eb0f390b64aa3fb8515cb
zengchangfu <zengchangfu@uhigame.com>
2024-07-19 10:59:27 +0800

    测试

--- Assets/3rd/XAsset/Editor/BuildRules.cs
+++ Assets/3rd/XAsset/Editor/BuildRules.cs
@@ -89,7 +89,7 @@ namespace libx
 					if ((ext == ".fbx" || ext == ".anim") && !litem.Contains(ext)) continue;
 					if (!BuildRules.ValidateAsset(file)) continue;
 					var asset = file.Replace("\\", "/");
-					if(getFiles.Contains(asset)) continue;
+					//if(getFiles.Contains(asset)) continue;
 					getFiles.Add(asset);
 				}
 			}
