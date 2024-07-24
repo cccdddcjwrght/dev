7f43a6d404ef28a4301f8df5dd9c389096e1d578
silekey <46124608+silekey2@users.noreply.github.com>
2024-07-24 14:31:07 +0800

    AI-fix-优化AI性能

---
+++ Assets/Script/Game/Module/VisualScript/Game/Character/HasIdleWorker.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+namespace SGame.VS
+{
+    [UnitTitle("HasIdleWorker")] // 查找点单工人
+    [UnitCategory("Game/Character")]
+    public sealed class HasIdleWorker : Unit
+    {
+        /// <summary>
+        /// The value of the variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueOutput value { get; private set; }
+
+        /// <summary>
+        /// 角色 MASK
+        /// </summary>
+        [DoNotSerialize]
+        public ValueInput m_roleTypeMask { get; private set; }
+        
+        [DoNotSerialize]
+        public ControlInput m_input;
+
+        [DoNotSerialize]
+        public ControlOutput m_yes;
+        
+        [DoNotSerialize]
+        public ControlOutput m_no;
+
+        private List<Character> m_cache = new List<Character>();
+
+        protected override void Definition()
+        {
+            m_roleTypeMask = ValueInput<int>("roleTypeMask", 5);
+
+            m_input = ControlInput("Input", (flow) =>
+            {
+                int roleTypeMask = flow.GetValue<int>(m_roleTypeMask);
+
+                CharacterModule.Instance.FindCharacters(m_cache, (character) => BitOperator.Get(roleTypeMask, character.roleType));             // (character.roleType == roleType1 || character.roleType == roleType2));
+
+                return m_cache.Count > 0 ? m_yes : m_no;
+            });
+
+            m_yes   = ControlOutput("yes");
+            m_no      = ControlOutput("no");
+        }
+    }
+}
