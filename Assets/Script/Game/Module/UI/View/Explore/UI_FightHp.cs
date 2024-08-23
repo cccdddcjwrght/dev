030d4af331fe6921c73c3efcf2182a992f07ef12
chenjiawang <chenjiawang@crowd-star.com>
2024-08-23 17:49:06 +0800

    战斗-add-血条ui

---
+++ Assets/Script/Game/Module/UI/View/Explore/UI_FightHp.cs
@@ -0,0 +1,25 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+using FairyGUI.Utils;
+
+namespace SGame.UI.Explore
+{
+    public partial class UI_FightHp : GProgressBar
+    {
+        public GTextField m_value;
+        public const string URL = "ui://ow12is1hkp2b26";
+
+        public static UI_FightHp CreateInstance()
+        {
+            return (UI_FightHp)UIPackage.CreateObject("Explore", "FightHp");
+        }
+
+        public override void ConstructFromXML(XML xml)
+        {
+            base.ConstructFromXML(xml);
+
+            m_value = (GTextField)GetChildAt(2);
+        }
+    }
+}
